85a4356c748cea3b1c275496f38d9babe4905b36
sachinpatel2707 <sachinpatel2707@gmail.com>
2020-04-19 22:10:45 +0530

    routing configured

--- quiz-app/src/app/app-routing.module.ts
+++ quiz-app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule, Router } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { QuizWindowComponent } from './components/quiz-window/quiz-window.component';
 
 const routes: Routes = [
-
+    { path: 'home', component: HeaderComponent },
+    { path: 'quiz-window', component: QuizWindowComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
 @NgModule({
--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -16,7 +17,8 @@ import { QuizWindowComponent } from './components/quiz-window/quiz-window.compon
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
